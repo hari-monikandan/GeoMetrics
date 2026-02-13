e63354415b4df5a010ad3d218f84dfd5cfb5256d
harimonikandan <hariprasath2002@gmail.com>
2026-02-12 19:02:22 -0500

    renamed vars and modified list comprehension

--- ellipse.py
+++ ellipse.py
@@ -1,18 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math as m
+import math
 
 a = input('What is the value of semi-major axis?')
 b = input('What is the value of semi-minor axis?')
 
 # Creating a list of x-values between (0,0) and (a,0). Total circumference is 4 times the first quadrant
 
-x = np.linspace(0,a,10000)
+x_values = np.linspace(0,a,10000)
 
 # Formula for an ellipse is y = sqrt(a^2 - x^2)*(b/a). So finding y values for corresponding x points.
 
-y = m.sqrt(a**2 - x**2)*(b/a)
+y_values = math.sqrt(a**2 - x**2)*(b/a)
 
 # Now need to order pairs (x,y) and find distances between every consecutive point.
 
-ordered_pair = [(i,j) for i in x for j in y]
+ordered_pair = [(x,y) for x,y in zip(x_values, y_values)]
