027a7f3e3e99945b6a26c6e52f562ee371878bdc
harimonikandan <hariprasath2002@gmail.com>
2026-02-12 19:04:34 -0500

    added major and minor

--- ellipse.py
+++ ellipse.py
@@ -5,13 +5,16 @@ import math
 a = input('What is the value of semi-major axis?')
 b = input('What is the value of semi-minor axis?')
 
+major = int(a)
+minor = int(a)
+
 # Creating a list of x-values between (0,0) and (a,0). Total circumference is 4 times the first quadrant
 
-x_values = np.linspace(0,a,10000)
+x_values = np.linspace(0,major,10000)
 
 # Formula for an ellipse is y = sqrt(a^2 - x^2)*(b/a). So finding y values for corresponding x points.
 
-y_values = math.sqrt(a**2 - x**2)*(b/a)
+y_values = math.sqrt(major**2 - x_values**2)*(minor/major)
 
 # Now need to order pairs (x,y) and find distances between every consecutive point.
 
