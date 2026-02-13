2c6a0c135ad2118f0be97661ed97d1bef9dad933
Dheepan Singaravel <dheepansingaravel7@gmail.com>
2026-02-13 00:13:05 +0000

    Ellipse Update Ordered Pairs

--- ellipse.py
+++ ellipse.py
@@ -10,12 +10,14 @@ minor = int(a)
 
 # Creating a list of x-values between (0,0) and (a,0). Total circumference is 4 times the first quadrant
 
-x_values = np.linspace(0,major,10000)
+x_values = np.linspace(0,major,10)
 
 # Formula for an ellipse is y = sqrt(a^2 - x^2)*(b/a). So finding y values for corresponding x points.
 
-y_values = math.sqrt(major**2 - x_values**2)*(minor/major)
+y_values = np.sqrt(major**2 - x_values**2)*(minor/major)
 
 # Now need to order pairs (x,y) and find distances between every consecutive point.
 
 ordered_pair = [(x,y) for x,y in zip(x_values, y_values)]
+
+print(ordered_pair)
