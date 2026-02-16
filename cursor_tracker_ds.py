3c5246a1e6867c1ff32c1075f993b46c0674b053
Dheepan Singaravel <dheepansingaravel7@gmail.com>
2026-02-16 00:00:37 +0000

    working model of tracker and grapher

--- cursor_tracker_ds.py
+++ cursor_tracker_ds.py
@@ -1,3 +1,21 @@
 # Need to write a code that can track (x,y) coordinates and find circumference in pixels.
 # There should also be an option to set a scale to get answer in physical units
  
+# The package pyautogui seems to be a nice method to retreive mouse positions.
+import pyautogui
+import matplotlib.pyplot as plt
+
+print('Press Ctrl-C to quit.')
+
+list_coordinates = []
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+        list_coordinates.append(tuple((x,y)))
+except KeyboardInterrupt:
+    plt.plot([x[0] for x in list_coordinates], [y[1] for y in list_coordinates])
+    plt.show()
+    print('\n')
