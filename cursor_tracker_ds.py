6e25e1433003e82db42f42923ab6ccb470c72625
Dheepan Singaravel <dheepansingaravel7@gmail.com>
2026-02-17 18:56:15 +0000

    circumference found for custom cursor

--- cursor_tracker_ds.py
+++ cursor_tracker_ds.py
@@ -4,6 +4,9 @@
 # The package pyautogui seems to be a nice method to retreive mouse positions.
 import pyautogui
 import matplotlib.pyplot as plt
+import numpy as np
+import keyboard
+import time
 
 print('Press Ctrl-C to quit.')
 
@@ -11,11 +14,20 @@ list_coordinates = []
 try:
     while True:
         x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-        print(positionStr, end='')
-        print('\b' * len(positionStr), end='')
-        list_coordinates.append(tuple((x,y)))
+        # positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        # print(positionStr, end='')
+        # print('\b' * len(positionStr), end='')
+        if keyboard.is_pressed('q'):
+            list_coordinates.append((x, y))
+
 except KeyboardInterrupt:
     plt.plot([(x[0]) for x in list_coordinates], [(1079 - y[1]) for y in list_coordinates])
     plt.show()
     print('\n')
+
+def distance_finder(point1, point2):
+    return np.sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
+
+small_dists = [distance_finder(list_coordinates[i], list_coordinates[i+1]) for i in range(0, len(list_coordinates)-1)]
+
+print(sum(small_dists))
